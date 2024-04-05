d0896d5a197e5a3aa7d97f7f1f65eb2cb9e619e4
leleca098 <123051123+leleca098@users.noreply.github.com>
2024-04-04 21:03:08 -0300

    Add files via upload

---
+++ cadastroCarro 2/cadastroCarro/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  cadastroCarro
+//
+//  Created by iOS Lab on 04/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nome: UITextField!
+    @IBOutlet weak var ano: UITextField!
+    @IBOutlet weak var marca: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func modelo(_ sender: Any) {
+    }
+    
+    @IBAction func cadastrar(_ sender: Any) {
+    }
+    
+    @IBAction func novo(_ sender: Any) {
+    }
+    @IBAction func usado(_ sender: Any) {
+    }
+    @IBOutlet weak var showList: UITextView!
+}
+func adicionarValores (){
+    guard let nomeString
+}
